3a14c601ff8bbb6b599e61428834903016b69842
alexeevdv <mail@alexeevdv.ru>
2015-12-16 15:38:24 +0300

    GridView widget

---
+++ src/GridView.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace alexeevdv\adminlte;
+
+class GridView extends \yii\grid\GridView
+{
+    public $layout = '
+        <div class="box">
+            <div class="box-header">
+            </div>
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-sm-12">
+                        {items}
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-5">{summary}</div>
+                    <div class="col-sm-7"><div class="dataTables_paginate">{pager}</div></div>
+                </div>
+            </div>
+        </div>
+    ';
+
+    public $summaryOptions = [
+        'class' => 'dataTables_info',
+    ];
+
+    public $tableOptions = [
+        'class' => 'table table-striped table-bordered dataTable',
+    ];
+
+    public function init()
+    {
+        parent::init();
+
+        \alexeevdv\adminlte\assets\GridView::register($this->getView());
+    }
+}
+
---
+++ src/assets/GridView.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace alexeevdv\adminlte\assets;
+
+class GridView extends \yii\web\AssetBundle
+{
+    public $sourcePath = "@vendor/alexeevdv/yii2-admin-lte/src";
+
+    public $css = [
+        'css/gridview.css'
+    ];
+
+    public $js = [];
+
+    public $depends = [];
+}
